81b30c99fab890b7fa08bfd1b032d9696987a3b9
Baka632 <stevemc123456@outlook.com>
2024-08-20 14:56:12 +0800

    添加项目文件。

---
+++ MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿// Love you forever, Eureka
+// Even if beyond the ending that I promised
+
+using Windows.Data.Xml.Dom;
+using Windows.UI.Notifications;
+using Windows.UI.Xaml.Controls;
+
+// https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x804 上介绍了“空白页”项模板
+
+namespace EurekaWatchesYouForever.Uwp
+{
+    /// <summary>
+    /// 可用于自身或导航至 Frame 内部的空白页。
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void OnPageLoaded(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            string tileXml = $@"
+                <tile>
+                    <visual>
+                        <binding template='TileWide'
+                                 hint-lockDetailedStatus1='尤里卡一直在看着你' 
+                                 hint-lockDetailedStatus2='不会离开。'>
+                        </binding>
+                    </visual>
+                </tile>";
+
+            XmlDocument tileDoc = new XmlDocument();
+            tileDoc.LoadXml(tileXml);
+
+            TileNotification tileNotification = new TileNotification(tileDoc);
+            TileUpdateManager.CreateTileUpdaterForApplication().Update(tileNotification);
+        }
+    }
+}
